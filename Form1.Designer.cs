e38dda34404dc3eaf2227cb8c9e18c76ea30bfd6
huy312100 <huy312100@gmail.com>
2021-04-18 12:15:41 +0700

    Push complete requirement

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Search_NoSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridViewTxtSearch = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTxtSearch)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(747, 47);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(94, 27);
+            this.searchBtn.TabIndex = 1;
+            this.searchBtn.Text = "Tìm kiếm";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.searchTextBox.Location = new System.Drawing.Point(301, 46);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(338, 27);
+            this.searchTextBox.TabIndex = 0;
+            // 
+            // dataGridViewTxtSearch
+            // 
+            this.dataGridViewTxtSearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTxtSearch.Location = new System.Drawing.Point(0, 180);
+            this.dataGridViewTxtSearch.Name = "dataGridViewTxtSearch";
+            this.dataGridViewTxtSearch.RowHeadersWidth = 51;
+            this.dataGridViewTxtSearch.RowTemplate.Height = 29;
+            this.dataGridViewTxtSearch.Size = new System.Drawing.Size(975, 417);
+            this.dataGridViewTxtSearch.TabIndex = 1;
+            this.dataGridViewTxtSearch.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.searchBtn);
+            this.panel1.Controls.Add(this.searchTextBox);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(976, 143);
+            this.panel1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Điện thoại",
+            "Laptop",
+            "Sách",
+            "Thời trang",
+            "Phương tiện"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.DropDownClosed += new System.EventHandler(this.comboBox1_DropDownClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 602);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridViewTxtSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTxtSearch)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.DataGridView dataGridViewTxtSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Search_NoSQL
+{
+    public partial class Form1 : Form
+    {
+        public class Product
+        {
+            //[BsonId]
+            //public ObjectId ID { get; set; }
+
+            public string name { get; set; }
+            public int price { get; set; }
+            public string type { get; set; }
+            public string decription { get; set; }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+            MongoClient dbClient = new MongoClient("mongodb://localhost");
+
+            var database = dbClient.GetDatabase("Tiki");
+            var collection = database.GetCollection<BsonDocument>("Product");
+            
+
+            //collection.Indexes.CreateOne(new CreateIndexModel<BsonDocument>(Builders<BsonDocument>.IndexKeys.Text.(x => )));
+            List<Product> Product = new List<Product>();
+            
+            //var data = collection.Find(Builders<BsonDocument>.Filter.Text("Iphone")).ForEachAsync(x => System.Diagnostics.Debug.WriteLine(x));
+
+            //collection.Find(new BsonDocument()).ForEachAsync(x => System.Diagnostics.Debug.WriteLine(x));
+            var data = collection.Find(Builders<BsonDocument>.Filter.Text(searchTextBox.Text.ToString())).ToList();
+            if (data.Count != 0)
+            {
+                dataGridViewTxtSearch.Visible = true;
+                foreach (var item in data)
+                {
+                    Product dataProduct = new Product();
+                    //dataProduct.ID = item[0].AsObjectId;
+                    dataProduct.name = item[1].ToString();
+                    dataProduct.price = item[2].ToInt32();
+                    dataProduct.type = item[3].ToString();
+                    dataProduct.decription = item[4].ToString();
+                    Product.Add(dataProduct);
+                }
+                dataGridViewTxtSearch.DataSource = Product;
+            }
+            else
+            {
+                data = collection.Find(Builders<BsonDocument>.Filter.Regex("name", new BsonRegularExpression(searchTextBox.Text.ToString(), "i"))).ToList();
+                if (data.Count != 0)
+                {
+                    dataGridViewTxtSearch.Visible = true;
+                    foreach (var item in data)
+                    {
+                        Product dataProduct = new Product();
+                        //dataProduct.ID = item[0].AsObjectId;
+                        dataProduct.name = item[1].ToString();
+                        dataProduct.price = item[2].ToInt32();
+                        dataProduct.type = item[3].ToString();
+                        dataProduct.decription = item[4].ToString();
+                        Product.Add(dataProduct);
+                    }
+                    dataGridViewTxtSearch.DataSource = Product;
+                }
+            }
+           
+        }
+
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://localhost");
+
+            var database = dbClient.GetDatabase("Tiki");
+            var collection = database.GetCollection<BsonDocument>("Product");
+
+            List<Product> Product = new List<Product>();
+
+            //var data = collection.Find(Builders<BsonDocument>.Filter.Text("Iphone")).ForEachAsync(x => System.Diagnostics.Debug.WriteLine(x));
+
+            var data = collection.Find(Builders<BsonDocument>.Filter.Eq("type",comboBox1.SelectedItem.ToString())).ToList();
+            if (data.Count != 0)
+            {
+                dataGridViewTxtSearch.Visible = true;
+                foreach (var item in data)
+                {
+                    Product dataProduct = new Product();
+                    //dataProduct.ID = item[0].AsObjectId;
+                    dataProduct.name = item[1].ToString();
+                    dataProduct.price = item[2].ToInt32();
+                    dataProduct.type = item[3].ToString();
+                    dataProduct.decription = item[4].ToString();
+                    Product.Add(dataProduct);
+                }
+                dataGridViewTxtSearch.DataSource = Product;
+            }
+            else
+            {
+                MessageBox.Show("Không thấy kết qủa tìm kiếm");
+            }
+        }
+    }
+}
